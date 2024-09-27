f107202ecfaea3b6a52176f2fa6fff04a8945fbb
Darkhitori <34727429+Darkhitori@users.noreply.github.com>
2024-09-26 23:51:57 -0400

    Add files via upload

---
+++ NameGenerator/GetNextRandomName.cs
@@ -0,0 +1,72 @@
+﻿// Author: E Aubain
+// Developer: Darkhitori
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Lexic;
+
+namespace Darkhitori.PlaymakerActions._NameGenerator
+{
+    using HutongGames.PlayMaker;
+
+    [ActionCategory("NameGenerator")]
+    [Tooltip("Will return a new procedurally generated name according to the syllables and rules of the dictionary.")]
+    public class GetNextRandomName : FsmStateAction
+    {
+        #region Variables
+        [RequiredField]
+        [CheckForComponent(typeof(NameGenerator))]
+        public FsmOwnerDefault gameObject;
+        
+        [UIHint(UIHint.Variable)]
+        public FsmString randomName;
+        
+        [Tooltip("Repeat every frame.")]
+        public bool everyFrame;
+        
+        NameGenerator nameGen;
+        #endregion
+        
+        #region Reset Values
+        public override void Reset()
+        {
+            gameObject = null;
+            randomName = null;
+            everyFrame = false;
+        }
+        #endregion
+        
+        #region Methods
+        // Code that runs on entering the state.
+        public override void OnEnter()
+        {
+            DoMethod();
+            if (!everyFrame)
+            {
+                Finish();
+            }
+        }
+
+        // Code that runs every frame.
+        public override void OnUpdate()
+        {
+            DoMethod();
+        }
+
+        void DoMethod()
+        {
+            var go = Fsm.GetOwnerDefaultTarget(gameObject);
+            if(go == null)
+            {
+                return;
+            }
+            
+            nameGen = go.GetComponent<NameGenerator>();
+            
+            randomName.Value = nameGen.GetNextRandomName();
+        }
+        #endregion
+    }
+
+}
---
+++ NameGenerator/NamesSourceClass.cs
@@ -0,0 +1,70 @@
+﻿// Author: E Aubain
+// Developer: Darkhitori
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Lexic;
+
+namespace Darkhitori.PlaymakerActions._NameGenerator
+{
+    using HutongGames.PlayMaker;
+
+    [ActionCategory("NameGenerator")]
+    [Tooltip("The name of a class inheriting from BaseNames and implementing its methods.")]
+    public class NamesSourceClass : FsmStateAction
+    {
+        #region Variables
+        [RequiredField]
+        [CheckForComponent(typeof(NameGenerator))]
+        public FsmOwnerDefault gameObject;
+        
+        public FsmString namesSourceClass;
+        
+        [Tooltip("Repeat every frame.")]
+        public bool everyFrame;
+        
+        NameGenerator nameGen;
+        #endregion
+        
+        #region Reset Values
+        public override void Reset()
+        {
+            gameObject = null;
+            namesSourceClass = null;
+            everyFrame = false;
+        }
+        #endregion
+        
+        #region Methods
+        // Code that runs on entering the state.
+        public override void OnEnter()
+        {
+            DoMethod();
+            if (!everyFrame)
+            {
+                Finish();
+            }
+        }
+
+        // Code that runs every frame.
+        public override void OnUpdate()
+        {
+            DoMethod();
+        }
+
+        void DoMethod()
+        {
+            var go = Fsm.GetOwnerDefaultTarget(gameObject);
+            if(go == null)
+            {
+                return;
+            }
+            
+            nameGen = go.GetComponent<NameGenerator>();
+            nameGen.namesSourceClass = namesSourceClass.Value;
+        }
+        #endregion
+    }
+
+}
